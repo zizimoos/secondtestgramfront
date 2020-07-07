b26c5b9a92f76fbd2c136c85a649c63a57f18f4e
zizimoos_iMac <zizimoos@gmail.com>
2020-07-07 12:49:40 +0900

    ...

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={Theme}>
       <GlobalStyles />
       hello
-    </>
+    </ThemeProvider>
   );
 }
 
