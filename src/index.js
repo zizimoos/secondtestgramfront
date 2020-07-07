875ca6d0f57957668f4c18bfeb835f3b9d74f140
zizimoos_iMac <zizimoos@gmail.com>
2020-07-07 20:18:54 +0900

    front end setUp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
+import Client from "./Apollo/Client";
+import { ApolloProvider } from "react-apollo-hooks";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={Client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
