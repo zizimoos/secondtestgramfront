71da76b9edaf7b8a067fee2fa30379462d848134
zizimoos_iMac <zizimoos@gmail.com>
2020-07-07 19:27:35 +0900

    ...

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
+import Router from "./Router";
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
-      <GlobalStyles />
-      hello
+      <>
+        <GlobalStyles />
+        <Router isLoggedIn={!false} />
+      </>
     </ThemeProvider>
   );
 }
