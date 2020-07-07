2a1ddbb616fd0367540a3b4a6d1cbd057129b1c7
zizimoos_iMac <zizimoos@gmail.com>
2020-07-08 00:25:17 +0900

    toastify & create Account

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import styled, { ThemeProvider } from "styled-components";
+import { useQuery } from "react-apollo-hooks";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
-import { useQuery } from "react-apollo-hooks";
+
 import Footer from "./Footer";
 
 const QUERY = gql`
@@ -30,6 +33,7 @@ function App() {
         <GlobalStyles />
         <Router isLoggedIn={isLoggedIn} />
         <Footer></Footer>
+        <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
       </Wrapper>
     </ThemeProvider>
   );
