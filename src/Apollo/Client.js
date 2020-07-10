54067dd79abab35dc031e5c7f58b89f02489b0d2
zizimoos_iMac <zizimoos@gmail.com>
2020-07-10 14:28:53 +0900

    6.2

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -1,19 +1,12 @@
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
+import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
-
-const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: "http://localhost:4000",
-});
-
 export default new ApolloClient({
-  cache,
-  link,
-  resolvers,
-});
-
-cache.writeData({
-  data: defaults,
+  uri: "http://localhost:4000",
+  clientState: {
+    defaults,
+    resolvers,
+  },
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
 });
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -70,12 +70,16 @@ const ME = gql`
 `;
 export default withRouter(({ history }) => {
   const search = useInput("");
-  const meQuery = useQuery(ME);
-  console.log(meQuery);
+  const { data } = useQuery(ME);
+  if (data !== undefined) {
+    console.log(data.me.username);
+  }
+
   const onSearchSubmit = (e) => {
     e.preventDefault();
     history.push(`/search?term=${search.value}`);
   };
+
   return (
     <Header>
       <HeaderWrapper>
@@ -96,9 +100,15 @@ export default withRouter(({ history }) => {
           <HeaderLink to="/notifications">
             <HeartEmpty />
           </HeaderLink>
-          <HeaderLink to="/username">
-            <User />
-          </HeaderLink>
+          {!data ? (
+            <HeaderLink to="/#">
+              <User />
+            </HeaderLink>
+          ) : (
+            <HeaderLink to={data.me.username}>
+              <User />
+            </HeaderLink>
+          )}
         </HeaderColumn>
       </HeaderWrapper>
     </Header>
