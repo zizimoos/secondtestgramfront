06fba4410dad50e09d44a7c5e54c46447897f5b7
zizimoos_iMac <zizimoos@gmail.com>
2020-07-14 21:43:09 +0900

    FRONTED:PROFILE DONE!

--- src/Routes/Profile/ProfilePresenter.js
+++ src/Routes/Profile/ProfilePresenter.js
@@ -6,6 +6,7 @@ import Avatar from "../../Components/Avatar";
 import FatText from "../../Components/FatText";
 import FollowButton from "../../Components/FollowButton";
 import SquarePost from "../../Components/SquarePost";
+import Button from "../../Components/Button";
 
 const Wrapper = styled.div`
   min-height: 100vh;
@@ -60,7 +61,7 @@ const Posts = styled.div`
   grid-gap: 10px;
 `;
 
-export default ({ loading, data }) => {
+export default ({ loading, data, logOut }) => {
   if (loading === true) {
     return (
       <Wrapper>
@@ -96,7 +97,11 @@ export default ({ loading, data }) => {
           <HeaderColumn>
             <UsernameRow>
               <Username>{username}</Username>{" "}
-              {!isSelf && <FollowButton isFollowing={isFollowing} id={id} />}
+              {isSelf ? (
+                <Button onClick={logOut} text="Log Out" />
+              ) : (
+                <FollowButton isFollowing={isFollowing} id={id} />
+              )}
             </UsernameRow>
             <Counts>
               <Count>
