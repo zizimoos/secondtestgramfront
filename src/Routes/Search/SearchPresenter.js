fd9fd210129eebfe965f1a6c1c9f638290921567
zizimoos_iMac <zizimoos@gmail.com>
2020-07-14 16:55:09 +0900

    FRONTEND:SEARCH

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -4,12 +4,25 @@ import PropTypes from "prop-types";
 import FatText from "../../Components/FatText";
 import Loader from "../../Components/Loader";
 import UserCard from "../../Components/UserCard";
+import SquarePost from "../../Components/SquarePost";
 
 const Wrapper = styled.div`
   height: 50vh;
 `;
+const Section = styled.div`
+  margin-bottom: 50px;
+  display: grid;
+  grid-gap: 25px;
+  grid-template-columns: repeat(4, 160px);
+  grid-template-rows: 160px;
+  grid-auto-rows: 160px;
+`;
 
-const Section = styled.div``;
+const PostSection = styled(Section)`
+  grid-template-columns: repeat(4, 200px);
+  grid-template-rows: 200px;
+  grid-auto-rows: 200px;
+`;
 
 const SearchPresenter = ({ searchTerm, loading, data }) => {
   if (searchTerm === undefined) {
@@ -33,29 +46,35 @@ const SearchPresenter = ({ searchTerm, loading, data }) => {
           ) : (
             data.searchUser.map((user) => (
               <UserCard
+                key={user.id}
                 username={user.username}
                 isFollowing={user.isFollowing}
-                url={user.url}
+                url={user.avatar}
                 isSelf={user.isSelf}
+                id={user.id}
               />
             ))
           )}
         </Section>
-        <Section>
+        <PostSection>
           {data.searchPost.length === 0 ? (
             <FatText text="No Posts Found" />
           ) : (
-            data.searchPost.map((post) => null)
+            data.searchPost.map((post) => (
+              <SquarePost
+                likeCount={post.likeCount}
+                commentCount={post.commentCount}
+                file={post.files[0]}
+              />
+            ))
           )}
-        </Section>
+        </PostSection>
       </Wrapper>
     );
   }
 };
-
 SearchPresenter.propTypes = {
   searchTerm: PropTypes.string,
   loading: PropTypes.bool,
 };
-
 export default SearchPresenter;
