3224d671771ad82658c30900180fc9b7bc2fec7b
zizimoos_iMac <zizimoos@gmail.com>
2020-07-23 15:44:22 +0900

    sorry

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -8,8 +8,8 @@ export default createGlobalStyle`
         box-sizing: border-box;
     }
     body{
-        /* background-color:${(props) => props.theme.bgColor}; */
-        background-color:red;
+        background-color:${(props) => props.theme.bgColor};
+        
         color:${(props) => props.theme.blackColor};
         font-size:14px;
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
